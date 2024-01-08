22e8d2b89ff680777cfa97120e21295056dd17a6
Michael Sanda <michael.sanda@gmail.com>
2024-01-08 05:49:28 -0500

    started motion_control ros2 pkg

--- scripts/oxbot_start.sh
+++ scripts/oxbot_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --runtime nvidia -it --rm --network=host --privileged -v /dev/bus/usb:/dev/bus/usb -v /home/oxbot_dev/git/oxbot/src:/root/src mjs161803/oxbot:latest /bin/bash
+docker run --runtime nvidia -it --rm --network=host --privileged -v /dev/bus/usb:/dev/bus/usb -v /home/oxbot_dev/git/oxbot:/root/oxbot mjs161803/oxbot:latest /bin/bash
