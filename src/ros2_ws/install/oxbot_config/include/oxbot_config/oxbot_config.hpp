65ff807d4e066677d52df6bee4effa4d1e0a79d1
Michael Sanda <michael.sanda@gmail.com>
2024-03-20 17:19:13 +0000

    serial initializing handshake

--- src/ros2_ws/install/oxbot_config/include/oxbot_config/oxbot_config.hpp
+++ src/ros2_ws/install/oxbot_config/include/oxbot_config/oxbot_config.hpp
@@ -6,7 +6,7 @@
 // Serial Port Configuration for current Oxbot hardware platform
 #define MC_FRONT_WHEELS_SERIAL_PATH "/dev/front-wheels"
 #define MC_REAR_WHEELS_SERIAL_PATH "/dev/rear-wheels"
-#define MC_SERIAL_FEEDBACK_MESSAGE_SIZE 144 // the feedback message from the hoverboard controller is 144 bytes long 
+#define MC_SERIAL_FEEDBACK_MESSAGE_SIZE 18 // the feedback message from the hoverboard controller is 144 bytes long 
 
 // Timing configurations
 #define MC_OUTPUT_FREQUENCY 2 // desired frequency of motion_controller output publisher, in Hz
