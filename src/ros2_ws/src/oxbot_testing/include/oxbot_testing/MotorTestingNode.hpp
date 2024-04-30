3820807efff45fc4c7208df65e2ef0c6e89b7c08
Michael Sanda <michael.sanda@gmail.com>
2024-04-30 09:23:27 +0000

    added: motor testing state machine

--- src/ros2_ws/src/oxbot_testing/include/oxbot_testing/MotorTestingNode.hpp
+++ src/ros2_ws/src/oxbot_testing/include/oxbot_testing/MotorTestingNode.hpp
@@ -10,14 +10,29 @@ class MotorTestingNode: public rclcpp::Node {
     
     private:
     // Members
+    enum state {starting=0, 
+                zero_twist=1, 
+                increasing_speed=2, 
+                decreasing_speed=4, 
+                returning_to_zero=8, 
+                increasing_steer=16, 
+                decreasing_steer=32};
+    state prev_state_;
+    state current_state_;
+    state next_state_;
     rclcpp::Subscription<oxbot_interfaces::msg::MotionControlOutput>::SharedPtr mc_output_subscription_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr test_twist_publisher_;    
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr test_twist_publisher_;
+    rclcpp::TimerBase::SharedPtr twist_publisher_timer_;
     int max_speed_;
     int min_speed_;
     int max_steer_;
     int min_steer_;
+    int speed_step_size_;
+    int speed_step_direction_;
     int current_speed_cmd_;
     int current_steer_cmd_;
+    bool speed_testing;
+    bool steer_testing;
     signed int fb_speed_cmd_;
     signed int fb_steer_cmd_;
     signed int fb_r_rpm_;
@@ -29,4 +44,5 @@ class MotorTestingNode: public rclcpp::Node {
 
     // Methods & Callbacks (CBs)
     void MCOutputSubscriptionCB(const oxbot_interfaces::msg::MotionControlOutput &);   
+    void pubTimerCB(void);
 };
--- src/ros2_ws/src/oxbot_testing/src/MotorTestingNode.cpp
+++ src/ros2_ws/src/oxbot_testing/src/MotorTestingNode.cpp
@@ -10,14 +10,25 @@ using std::placeholders::_1;
 
 MotorTestingNode::MotorTestingNode() : Node("motor_tester")
 {
+    current_state_ = starting;
+    prev_state_ = starting;
+    next_state_ = starting;
+
     mc_output_subscription_ = this->create_subscription<oxbot_interfaces::msg::MotionControlOutput>("motor_controller_feedback", 1, std::bind(&MotorTestingNode::MCOutputSubscriptionCB, this, _1));
     test_twist_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("motion_cmd", 10);
+    int twist_publisher_timer_period_micros    = int(1000000* (1.0 / 0.2)); // 0.2 Hz... i.e. - every 5 seconds.
+    twist_publisher_timer_ =   this->create_wall_timer(std::chrono::microseconds(twist_publisher_timer_period_micros), std::bind(&MotorTestingNode::pubTimerCB, this));
+    
     int max_speed_=100;
     int min_speed_=-100;
     int max_steer_=100;
     int min_steer_=-100;
+    int step_size_=5;
+    int step_direction_=1;
     int current_speed_cmd_=0;
     int current_steer_cmd_=0;
+    bool speed_testing = true;
+    bool steer_testing = false;
 
     signed int fb_speed_cmd_=0;
     signed int fb_steer_cmd_=0;
@@ -38,4 +49,74 @@ void MotorTestingNode::MCOutputSubscriptionCB(const oxbot_interfaces::msg::Motio
     fb_v_batt_ = msg.mc_output.end()->batt_voltage_x100;
     fb_temperature_ = msg.mc_output.end()->temperature;
     fb_timestamp_ = msg.mc_output.end()->timestamp_ns;
+}
+
+void MotorTestingNode::pubTimerCB(void)
+{
+    switch (current_state_)
+    {
+    case starting:
+        /* code */
+        prev_state_ = current_state_;
+        current_state_ = increasing_speed;
+        break;
+    case zero_twist:
+        if (prev_state_ == increasing_speed | prev_state_ == decreasing_speed)
+        {
+            prev_state_ = current_state_;
+            current_state_ = increasing_steer;
+        }
+        else if (prev_state_ == increasing_steer | prev_state_ == decreasing_steer)
+        {
+            prev_state_ = current_state_;
+            current_state_ = increasing_speed;
+        }
+        break;
+    case increasing_speed:
+        current_speed_cmd_ += speed_step_size_*speed_step_direction_;
+        if (current_speed_cmd_ > max_speed_)
+        {
+            current_speed_cmd_ = max_speed_;
+            speed_step_direction_ *= -1;
+            prev_state_ = current_state_;
+            current_state_ = decreasing_speed;
+        }
+        else
+        {
+            prev_state_ = current_state_;
+            current_state_ = increasing_speed;
+        }
+        break;
+    case decreasing_speed:
+        current_speed_cmd_ += speed_step_size_*speed_step_direction_;
+        if (current_speed_cmd_ < min_speed_)
+        {
+            current_speed_cmd_ = min_speed_;
+            speed_step_direction_ *= -1;
+            prev_state_ = current_state_;
+            current_state_ = returning_to_zero;
+        }
+        else
+        {
+            prev_state_ = current_state_;
+            current_state_ = decreasing_speed;
+        }        
+        break;
+    case returning_to_zero:
+        //prev_state_ = current_state_;
+        break;
+    case increasing_steer:
+        //prev_state_ = current_state_;
+        break;
+    case decreasing_steer:
+        //prev_state_ = current_state_;
+        break;
+    default:
+        //prev_state_ = current_state_;
+        break;
+    }
+
+    geometry_msgs::msg::Twist new_mc_cmd;
+    new_mc_cmd.linear.x = current_speed_cmd_;
+    new_mc_cmd.angular.z = current_steer_cmd_;
 }
