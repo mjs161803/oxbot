a88acffaa366076c48c7455c2bd4cbb1c320f7f6
Michael Sanda <michael.sanda@gmail.com>
2024-07-26 20:14:08 +0000

    updated max speed

--- src/ros2_ws/src/oxbot_config/include/oxbot_config/oxbot_config.hpp
+++ src/ros2_ws/src/oxbot_config/include/oxbot_config/oxbot_config.hpp
@@ -11,6 +11,7 @@
 #define MC_REAR_WHEELS_INSTALL_ORIENTATION -1 // 1 for correct, -1 if hoverboard wheels were installed backwards
 #define MC_FRONT_WHEEL_DIAMETER_CM 19.0
 #define MC_REAR_WHEEL_DIAMETER_CM 24.5
+#define MC_MAX_SPEED_CM_PER_SEC 100
 
 // Timing configurations
 #define MC_OUTPUT_FREQUENCY 2 // desired frequency of motion_controller output publisher, in Hz
