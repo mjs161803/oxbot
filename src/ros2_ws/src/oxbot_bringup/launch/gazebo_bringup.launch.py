a5285c2902c201121a3773cfc941d741566343fb
Michael Sanda <michael.sanda@gmail.com>
2024-12-09 19:36:06 -0500

    working on adding gazebo sim

---
+++ src/ros2_ws/src/oxbot_bringup/launch/gazebo_bringup.launch.py
@@ -0,0 +1,64 @@
+import os
+from pathlib import Path 
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    oxbot_description_dir = get_package_share_directory("oxbot_description")
+    
+    model_arg = DeclareLaunchArgument(
+        name="model", 
+        default_value=os.path.join(oxbot_description_dir, "urdf", "oxbot.urdf.xacro"),
+        description="Absolute path to robot urdf file"
+    )
+    
+    robot_description = ParameterValue(
+        Command(["xacro ", LaunchConfiguration("model")]), 
+        value_type=str
+    )
+    
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}]
+    )
+        
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[
+            str(Path(oxbot_description_dir).parent.resolve())
+        ]
+    )
+    
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory("ros_gz_sim"), "launch"),
+            "/gz_sim.launch.py"
+        ]),
+        launch_arguments=[
+            ("gz_args", [" -v 4", " -r", " empty.sdf"])
+        
+        ]
+    )
+    
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description", "-name", "oxbot"]
+    )
+    
+    return LaunchDescription([
+        model_arg,
+        robot_state_publisher_node,
+        gazebo_resource_path,
+        gazebo,
+        gz_spawn_entity
+    ])
+    
+# be sure to update package.xml for oxbot_bringup: <exec_depend>ros_gz_sim</exec_depend>
