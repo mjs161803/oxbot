1f2fe24cc0322f4742dfce71424fe251f2bc31e6
Michael Sanda <michael.sanda@gmail.com>
2025-01-29 05:49:04 -0500

    added sensors to gazebo sim

--- src/ros2_ws/src/oxbot_bringup/launch/gazebo_bringup.launch.py
+++ src/ros2_ws/src/oxbot_bringup/launch/gazebo_bringup.launch.py
@@ -1,23 +1,42 @@
 import os
+from os import pathsep
 from pathlib import Path 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     oxbot_description_dir = get_package_share_directory("oxbot_description")
-    ros_distro = os.environ["ROS_DISTRO"]
-    is_ignition = "True" if ros_distro == "humble" else "False"
     
     model_arg = DeclareLaunchArgument(
         name="model", 
         default_value=os.path.join(oxbot_description_dir, "urdf", "oxbot.urdf.xacro"),
         description="Absolute path to robot urdf file"
     )
+    
+    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="empty")
+
+    world_path = PathJoinSubstitution([
+            oxbot_description_dir,
+            "worlds",
+            PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"])
+        ]
+    )
+
+    model_path = str(Path(oxbot_description_dir).parent.resolve())
+    model_path += pathsep + os.path.join(get_package_share_directory("oxbot_description"), 'models')
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH",
+        model_path
+        )
+    
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "humble" else "False"
     
     robot_description = ParameterValue(
         Command([
@@ -32,25 +51,18 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}]
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
     )
         
-    gazebo_resource_path = SetEnvironmentVariable(
-        name="GZ_SIM_RESOURCE_PATH",
-        value=[
-            str(Path(oxbot_description_dir).parent.resolve())
-        ]
-    )
-    
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             os.path.join(get_package_share_directory("ros_gz_sim"), "launch"),
             "/gz_sim.launch.py"
         ]),
-        launch_arguments=[
-            ("gz_args", [" -v 4", " -r", " empty.sdf"])
-        
-        ]
+        launch_arguments={
+            "gz_args": PythonExpression(["'", world_path, " -v 4 -r'"])
+        }.items()
     )
     
     gz_spawn_entity = Node(
@@ -104,13 +116,28 @@ def generate_launch_description():
         package="oxbot_gz_controller",
         executable="oxbot_teleop_node"
     )
+
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "imu@sensor_msgs/msg/Imu[gz.msgs.IMU",
+            "/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan"
+        ],
+        remappings=[
+            ('/imu', '/realsense/camera/imu'),
+        ]
+    )
     
     return LaunchDescription([
         model_arg,
+        world_name_arg,
         robot_state_publisher_node,
         gazebo_resource_path,
         gazebo,
         gz_spawn_entity,
+        gz_ros2_bridge,
         joint_state_broadcaster_spawner,
         wheel_controller,
         gz_motion_controller,
