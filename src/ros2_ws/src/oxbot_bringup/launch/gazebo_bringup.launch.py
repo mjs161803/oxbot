316a80ab990c6c24053ca7afda3cf16695f94ade
Michael Sanda <michael.sanda@gmail.com>
2025-02-14 13:31:04 -0500

    adding mapping package

--- src/ros2_ws/src/oxbot_bringup/launch/gazebo_bringup.launch.py
+++ src/ros2_ws/src/oxbot_bringup/launch/gazebo_bringup.launch.py
@@ -18,12 +18,12 @@ def generate_launch_description():
         description="Absolute path to robot urdf file"
     )
     
-    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="empty")
+    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="my_house")
 
     world_path = PathJoinSubstitution([
             oxbot_description_dir,
             "worlds",
-            PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"])
+            PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.sdf'"])
         ]
     )
 
